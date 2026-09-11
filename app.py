d03f04847ac251fb9ef2cd979dd6bda8418746e1
aobedonazarik-commits <aobedonazarik@gmail.com>
2026-09-11 19:53:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# Разрешаем нашему Mini App обращаться к бэкенду
+CORS(app)
 
 
 @app.route("/")
@@ -22,9 +26,17 @@ def webapp():
 
     return jsonify({
         "success": True,
+        "message": "Mini App успешно связался с Backend!",
         "received": data
     })
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
