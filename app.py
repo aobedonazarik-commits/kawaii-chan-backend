56e51129e36f509841cd7c886ba0cdf98e884698
aobedonazarik-commits <aobedonazarik@gmail.com>
2026-09-17 02:11:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2058,12 +2058,12 @@ def create_album():
                     )
 
 
-             if file_size <= 0:
+              if file_size <= 0:
                 raise RuntimeError(
                     f"Страница #{position} пустая"
                 )
 
-             if file_size > MAX_ALBUM_FILE_SIZE:
+              if file_size > MAX_ALBUM_FILE_SIZE:
                 raise RuntimeError(
                     f"Страница #{position} "
                     f"слишком большая. "
